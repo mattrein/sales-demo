95d2527eb19676af9c1c70a02f28040f43dc1903
Matt Rein <m.rein@ionic.io>
2023-12-29 11:20:52 -0600

    Add Color shade and tint. Updated picker styles

--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -4,21 +4,23 @@ import { fromEvent, map, startWith } from 'rxjs';
 
 export interface Color {
   name: string;
-  order: number;
+  colorId: number;
   swatch: number;
   hex: string;
+  shade?: string;
+  tint?: string;
 }
 
 const COLOR_DATA: Color[] = [
-  {order: 1, name: 'Primary', swatch: 1.0079, hex: '#3880ff'},
-  {order: 2, name: 'Secondary', swatch: 4.0026, hex: '#3dc2ff'},
-  {order: 3, name: 'Tertiary', swatch: 6.941, hex: '#5260ff'},
-  {order: 4, name: 'Success', swatch: 9.0122, hex: '#2dd36f'},
-  {order: 5, name: 'Warning', swatch: 10.811, hex: '#ffc409'},
-  {order: 6, name: 'Danger', swatch: 12.0107, hex: '#eb445a'},
-  {order: 7, name: 'Light', swatch: 14.0067, hex: '#f4f5f8'},
-  {order: 8, name: 'Medium', swatch: 15.9994, hex: '#92949c'},
-  {order: 9, name: 'Dark', swatch: 18.9984, hex: '#222428'},
+  {colorId: 1, name: 'Primary', swatch: 1.0079, hex: '#3880ff'},
+  {colorId: 2, name: 'Secondary', swatch: 4.0026, hex: '#3dc2ff'},
+  {colorId: 3, name: 'Tertiary', swatch: 6.941, hex: '#5260ff'},
+  {colorId: 4, name: 'Success', swatch: 9.0122, hex: '#2dd36f'},
+  {colorId: 5, name: 'Warning', swatch: 10.811, hex: '#ffc409'},
+  {colorId: 6, name: 'Danger', swatch: 12.0107, hex: '#eb445a'},
+  {colorId: 7, name: 'Light', swatch: 14.0067, hex: '#f4f5f8'},
+  {colorId: 8, name: 'Medium', swatch: 15.9994, hex: '#92949c'},
+  {colorId: 9, name: 'Dark', swatch: 18.9984, hex: '#222428'},
 ];
 
 /**
@@ -32,8 +34,13 @@ const COLOR_DATA: Color[] = [
   imports: [MatTableModule],
 })
 export class ColorsComponent {
-  displayedColumns: string[] = ['order', 'name', 'swatch', 'hex'];
-  dataSource = COLOR_DATA;
+  displayedColumns: string[] = ['colorId', 'name', 'swatch', 'hex', 'shade', 'tint'];
+  dataSource = COLOR_DATA.map(c => {
+      let {shade, tint} = this.shadeAndTint(c.hex);
+      c.shade = shade;
+      c.tint = tint;
+      return c
+    });
 
   ngOnInit() {
     fromEvent(window, 'resize')
@@ -44,8 +51,29 @@ export class ColorsComponent {
         if(width <= 768) {
           this.displayedColumns = ['name', 'swatch'];
         } else {
-          this.displayedColumns = ['order', 'name', 'swatch', 'hex'];
+          this.displayedColumns = ['colorId', 'name', 'swatch', 'hex', 'shade', 'tint'];
         }
       });
   }
+
+  updateColors(event: Event, colorId: number) {
+    const newColor = (event.target as HTMLInputElement).value;
+    let index = this.dataSource.findIndex(c => c.colorId === colorId);
+    this.dataSource[index].hex = newColor;
+    let {shade, tint} = this.shadeAndTint(this.dataSource[index].hex);
+    this.dataSource[index].shade = shade;
+    this.dataSource[index].tint = tint;
+  }
+
+  shadeAndTint(color: string, amount: number = 60) {
+    const colorValue = parseInt(color.slice(1), 16);
+    const r = (colorValue >> 16) & 255;
+    const g = (colorValue >> 8) & 255;
+    const b = colorValue & 255;
+
+    const shade = "#" + ((1 << 24) + (Math.max(0, r - amount) << 16) + (Math.max(0, g - amount) << 8) + Math.max(0, b - amount)).toString(16).slice(1);
+    const tint = "#" + ((1 << 24) + (Math.min(255, r + amount) << 16) + (Math.min(255, g + amount) << 8) + Math.min(255, b + amount)).toString(16).slice(1);
+
+    return { shade, tint };
+}
 }
